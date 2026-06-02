2b57356b304b14f110294f18d1135f760bbc2379
Omollo-George <omollogeorge096@gmail.com>
2026-06-02 10:02:56 +0300

    Add my new code changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Set root to project directory where index.html is located
-  root: './',
+  // Point Vite to the folder containing your frontend entry point (index.html)
+  root: 'client', 
   plugins: [react()],
   build: {
-    // Output the build artifacts to 'dist'
-    outDir: 'dist',
+    // Output the build artifacts to 'dist' in the root project folder
+    outDir: '../dist',
     // Ensure the output directory is cleared before building
     emptyOutDir: true,
   },
