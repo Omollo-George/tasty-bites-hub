132a68375215ee5469687db04edf90bfc67f908d
omoll <omollogeorge096@gmail.com>
2026-06-26 14:00:45 +0300

    update:added items

--- frontend/src/components/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatCurrency = void 0;
-var formatCurrency = function (value, currencyCode) {
-    if (currencyCode === void 0) { currencyCode = "KES"; }
-    var locale = currencyCode === "KES" ? "en-KE" : "en-US";
-    return new Intl.NumberFormat(locale, { style: "currency", currency: currencyCode }).format(value);
-};
-exports.formatCurrency = formatCurrency;
